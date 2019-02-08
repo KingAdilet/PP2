c7e7264070f13d4348ac4be0d7990e3d11337d5e
KingAdilet <adilet292@gmail.com>
2019-02-08 13:19:04 +0600

    task4

---
+++ week1/TASK 4/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            //input the number for array
+            int n = int.Parse(s);
+            //for number give equivalent bit-number
+            for (int i = 0; i < n; i++)//declaring cycle for array
+            {
+                for (int j = 0; j < i + 1; j++)//add second dimension to array 
+                {
+                    Console.Write("[*}");//filling 
+                }
+                Console.WriteLine();//output 
+            }
+            //typical code for creating and filling array with [*]
+
+            Console.ReadKey();//to make console be closed after pushing any button
+
+        }
+    }
+}
