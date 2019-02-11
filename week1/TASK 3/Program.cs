898842b26ae9f57c10c1e89eb277d98ce444dfe2
KingAdilet <adilet292@gmail.com>
2019-02-12 05:09:18 +0600

    adding

---
+++ week1/TASK 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_3.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            //create an array and simultaneously convert it to binary
+            string[] arr = Console.ReadLine().Split();
+            //            fill array avoiding " "-gaps 
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(arr[i]);
+            }
+            //fill array  every ячейка one by one and converting it
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a[i] + " " + a[i] + " ");
+            }
+            //Output in console
+
+            Console.ReadKey();//close console window with pushing any button
+        }
+    }
+}
---
+++ week2/TASK1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePolindrome
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            string s = string.Empty; //Represents the empty string. This field is read-only.
+            Console.WriteLine("Enter");
+            s = Console.ReadLine(); 
+            //fill string 
+            char[] temp = s.ToCharArray();//to array
+            Array.Reverse(temp); //reverses
+            string b = new string(temp); //assigning
+            // convert it into array and reverse
+
+            if (s.Equals(b))//compares it reverse version with original
+            {
+                Console.WriteLine("POLINDROME");
+            }
+            else
+            {
+                Console.WriteLine("NOT POLINDROME");
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
+ 
